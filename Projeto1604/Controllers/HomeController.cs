147a6d63ac9dee035c91ddbd9dd23dcc5dbf5621
evndroo <evandro.slipjr@gmail.com>
2020-09-01 16:00:44 -0300

    Adding annotations on the model and showing the erros on the View

---
+++ Projeto1604/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Projeto1604.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto1604.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Questionario()
+        {
+
+            return View();
+        }
+
+        public ActionResult Obrigado()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Questionario(QuestionarioModel resposta) {
+            try {
+                return RedirectToAction("Obrigado");
+            }
+            catch {
+                return View();
+            }
+        }
+    }
+}
---
+++ Projeto1604/Models/QuestionarioModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Projeto1604.Models
+{
+    public class QuestionarioModel
+    {
+        [Required]
+        [DisplayName("Tipo de projeto")]
+        public string TipoProjeto { get; set; }
+
+        [Required(ErrorMessage ="O CEP deve ser informado")]
+        [RegularExpression(@"^\d{8}$|^\d{5}-\d{3}$",ErrorMessage ="O formato de código postal é 00000000 ou 000000-000")]
+        [DisplayName("CEP")]
+        public string Cep { get; set; }
+
+        [Required(ErrorMessage ="O contato deve ser informado")]
+        [StringLength(50,MinimumLength =5)]
+        public string Contato { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="O telefone deve ser informado")]
+        [DataType(DataType.PhoneNumber,ErrorMessage ="Forneça o número de telefone no formato (00)000-0000")]
+        [DisplayName("Número de Telefone")]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage ="Descrição é obrigatório")]
+        [StringLength(5000, MinimumLength =20)]
+        [DataType(DataType.MultilineText)]
+        [DisplayName("Descrição do projeto")]
+        public string Descricao { get; set; }
+
+    }
+}
